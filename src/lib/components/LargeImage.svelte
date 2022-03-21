0e23aa5ee2b0933052ca1685966848237a288d02
scottBowles <shbowles@gmail.com>
2022-03-20 20:30:49 -0400

    Add LargeImage component

---
+++ src/lib/components/LargeImage.svelte
@@ -0,0 +1,24 @@
+<script>
+	/** Cloudinary actions */
+	import { thumbnail } from '@cloudinary/url-gen/actions/resize';
+	import { byRadius } from '@cloudinary/url-gen/actions/roundCorners';
+
+	/** Cloudinary instance store */
+	import cloudinary from '$lib/stores/cloudinary';
+
+	export let imageId = 'dnd/sfaedxiltuowlw7whb0c';
+	export let width = 400;
+	export let height = 'auto';
+	export let radius = 0;
+	export let alt = '';
+
+	let src;
+
+	const image = $cloudinary.image(imageId);
+	image.resize(thumbnail().width(width)).roundCorners(byRadius(radius));
+	src = image.toURL();
+</script>
+
+{#if src}
+	<img {src} {alt} />
+{/if}
