b3a98ca8f807bd174f05377f9da10e3a3c02408a
scottBowles <shbowles@gmail.com>
2022-03-20 20:32:44 -0400

    Get association detail page to an mvp level

--- src/routes/associations/[id].svelte
+++ src/routes/associations/[id].svelte
@@ -4,18 +4,41 @@
 		associationById as queriedAssociation
 	} from '$lib/graphql/AssociationQueries.gq';
 
-	export async function load({ fetch, params }) {
-		const { id } = params;
-		const a = await getAssociationById({ fetch, variables: { id } });
-		return {};
-	}
+	export const load = async ({ fetch, params }) =>
+		await getAssociationById({ fetch, variables: { id: params.id } });
 </script>
 
 <script>
-	import { Heading, Text } from '@kahi-ui/framework';
+	import { Container, Heading, Text } from '@kahi-ui/framework';
+
+	import LargeImage from '$lib/components/LargeImage.svelte';
 
 	$: association = $queriedAssociation?.association;
 </script>
 
-<Heading is="h1">{association.name}</Heading>
-<Text>Description: {association.description}</Text>
+<div class="spacer" />
+<Container>
+	<Heading is="h1">{association.name}</Heading>
+	<div class="spacer" />
+	<div class="img-container">
+		<LargeImage alt={association.name} />
+	</div>
+	<Heading is="h3">Description</Heading>
+	<div class="spacer-sm" />
+	<Text>{association.description}</Text>
+</Container>
+
+<style>
+	.spacer {
+		height: 2em;
+	}
+	.spacer-sm {
+		height: 1em;
+	}
+	.img-container {
+		float: right;
+		width: clamp(40%, (120px - 40%) * 1000, 100%);
+		margin-left: 0.5em;
+		margin-bottom: 0.5em;
+	}
+</style>
