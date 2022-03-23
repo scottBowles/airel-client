ee89a2f0cbcddf8d940b22fbdb00e3d5d61a066d
scottBowles <shbowles@gmail.com>
2022-03-22 20:08:40 -0400

    Use different api paths depending on environment mode

--- src/lib/config/g.ts
+++ src/lib/config/g.ts
@@ -1,6 +1,8 @@
 import { GFetch } from '@leveluptuts/g-query';
 
-export const g = new GFetch({
-	path: 'http://127.0.0.1:8000/graphql/' //whatever your api url is here
-	// More config options coming, for now this is just path to your graphql api
-});
+const isProd = import.meta.env.PROD;
+const PROD_PATH = 'https://airel.onrender.com/graphql/';
+const DEV_PATH = 'http://127.0.0.1:8000/graphql/';
+const path = isProd ? PROD_PATH : DEV_PATH;
+
+export const g = new GFetch({ path });
--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,11 @@ import preprocess from 'svelte-preprocess';
 
 import gQueryCodegen from '@leveluptuts/g-query/codegen';
 
+const isProd = process.env.NODE_ENV === 'production';
+const PROD_SCHEMA_PATH = 'https://airel.onrender.com/graphql/schema';
+const DEV_SCHEMA_PATH = 'http://127.0.0.1:8000/graphql/schema/';
+const SCHEMA_PATH = isProd ? PROD_SCHEMA_PATH : DEV_SCHEMA_PATH;
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -14,10 +19,7 @@ const config = {
 		vite: {
 			plugins: [
 				gQueryCodegen({
-					// Required
-					// schema: 'http://localhost:3001/graphql' // this can also be a url to a graphql api
-					schema: 'http://127.0.0.1:8000/graphql/schema/', // path to schema, schema is required
-					// schema: './src/lib/config/schema.graphql', // path to schema, schema is required
+					schema: SCHEMA_PATH, // path to schema, schema is required
 					out: './src/lib/graphql', // Where you want the general schema types to output
 					gPath: '$lib/config/g', // Path to g, created in step 1.
 					// Optional
