c5551bd53f976de531d0a7611bae4c43f20082ea
scottBowles <shbowles@gmail.com>
2022-05-02 01:48:50 -0400

    Add CloudinaryUpload

---
+++ src/lib/components/CloudinaryUpload.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let handleImageUpload;
+	let uploadWidget;
+	let openWidget;
+
+	onMount(() => {
+		uploadWidget = window?.cloudinary?.createUploadWidget(
+			{
+				cloudName: 'scottBowles',
+				uploadPreset: 'ubste3oi'
+			},
+			handleImageUpload
+		);
+
+		openWidget = () => uploadWidget && uploadWidget.open();
+	});
+</script>
+
+<svelte:head>
+	<script
+		src="https://upload-widget.cloudinary.com/global/all.js"
+		type="text/javascript"
+		async></script>
+</svelte:head>
+
+<span on:click={openWidget}>
+	<slot>this is a slot for a component which will open the cloudinary upload widget on click</slot>
+</span>
+
+<style>
+	span {
+		cursor: pointer;
+	}
+</style>
