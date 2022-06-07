15424e685e961ec4faf8997a54d064e1cb7776a3
scottBowles <shbowles@gmail.com>
2022-06-06 20:42:25 -0400

    Use lifecycle methods instead of action and import quill dynamically

--- src/lib/components/QuillEditor.svelte
+++ src/lib/components/QuillEditor.svelte
@@ -1,65 +1,59 @@
 <script>
-	import { browser } from '$app/env';
-	import Quill from 'quill';
+	import { onDestroy, onMount } from 'svelte';
 
 	export let options = { placeholder: 'Anything goes here...' };
 	export let html = '<p>Initial content</p>';
+	let node;
 
-	function quill(node, { options = {}, initialHtml }) {
-		if (browser) {
-			const quill = new Quill(node, {
-				modules: {
-					toolbar: [
-						[{ header: [1, 2, 3, false] }],
-						['bold', 'italic', 'underline', 'strike'],
-						['link', 'code-block']
-					]
-				},
-				placeholder: 'Type something...',
-				theme: 'snow', // or 'bubble'
-				...options
-			});
+	onMount(async () => {
+		const { default: Quill } = await import('quill');
 
-			if (initialHtml) {
-				const delta = quill.clipboard.convert(initialHtml);
-				quill.setContents(delta);
-			}
+		let quill = new Quill(node, {
+			modules: {
+				toolbar: [
+					[{ header: [1, 2, 3, false] }],
+					['bold', 'italic', 'underline', 'strike'],
+					['link', 'code-block']
+				]
+			},
+			placeholder: 'Type something...',
+			theme: 'snow', // or 'bubble'
+			...options
+		});
 
-			const container = node.getElementsByClassName('ql-editor')[0];
+		const container = node.getElementsByClassName('ql-editor')[0];
 
-			quill.on('text-change', function (/*delta, oldDelta, source*/) {
-				node.dispatchEvent(
-					new CustomEvent('text-change', {
-						detail: {
-							html: container.innerHTML,
-							text: quill.getText(),
-							contents: quill.getContents()
-						}
-					})
-				);
-			});
-
-			return {
-				destroy() {
-					if (document) {
-						const els = document.querySelectorAll("div[class^='ql-']");
-						for (const el of els) {
-							if (el.parentNode !== null) {
-								el.parentNode.removeChild(el);
-							}
-						}
+		quill.on('text-change', function (/*delta, oldDelta, source*/) {
+			node.dispatchEvent(
+				new CustomEvent('text-change', {
+					detail: {
+						html: container.innerHTML,
+						text: quill.getText(),
+						contents: quill.getContents()
 					}
+				})
+			);
+		});
+	});
+
+	onDestroy(() => {
+		node.removeEventListener('text-change', handleTextChange);
+		if (document) {
+			const els = document.querySelectorAll("div[class^='ql-']");
+			for (const el of els) {
+				if (el.parentNode !== null) {
+					el.parentNode.removeChild(el);
 				}
-			};
+			}
 		}
-	}
+	});
 
 	function handleTextChange(e) {
 		html = e.detail.html;
 	}
 </script>
 
-<div class="editor" use:quill={{ options, initialHtml: html }} on:text-change={handleTextChange} />
+<div class="editor" bind:this={node} on:text-change={handleTextChange} />
 
 <style>
 	@import 'https://cdn.quilljs.com/1.3.6/quill.snow.css';
