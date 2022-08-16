81751e75b842e03704e2bf973e02ee99db95e5c3
scottBowles <shbowles@gmail.com>
2022-08-16 00:59:12 -0400

    Bring sveltekit up to @next.405 (latest before big changes)

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
-import watchAndRun from '@kitql/vite-plugin-watch-and-run';
-
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -16,17 +12,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
-		vite: {
-			plugins: [
-				watchAndRun([
-					{
-						watch: '**/*.(gql|graphql)',
-						run: isProd ? 'npm run gen' : 'npm run gen:dev'
-					}
-				])
-			]
-		}
+		adapter: adapter()
 	}
 };
 
