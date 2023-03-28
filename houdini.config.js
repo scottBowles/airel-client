7a9019bcc36e8d3d000fc25407153d631673eed1
scottBowles <shbowles@gmail.com>
2023-03-28 00:48:00 -0400

    Houdini init

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "http://127.0.0.1:8000/graphql/"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
