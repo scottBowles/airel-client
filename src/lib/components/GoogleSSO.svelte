8dba31e171a9c3a6a8b2f8076a849c5a4b3e724d
scottBowles <shbowles@gmail.com>
2023-03-31 20:09:42 -0400

    Ensure google login button loads on the page each time

---
+++ src/lib/components/GoogleSSO.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const script = document.createElement('script');
+		script.src = 'https://accounts.google.com/gsi/client';
+		script.async = true;
+		script.defer = true;
+		document.body.appendChild(script);
+		return () => {
+			document.body.removeChild(script);
+		};
+	});
+</script>
+
+<div
+	id="g_id_onload"
+	data-client_id="663709274997-73tvumrgtdluvolar09ifg9vcehrdfcg.apps.googleusercontent.com"
+	data-context="signin"
+	data-ux_mode="popup"
+	data-login_uri="http://localhost:5173/endpoints/google_auth"
+	data-auto_select="true"
+	data-itp_support="true"
+/>
+
+<div
+	class="g_id_signin"
+	data-type="standard"
+	data-shape="rectangular"
+	data-theme="outline"
+	data-text="signin_with"
+	data-size="large"
+	data-logo_alignment="left"
+	style="z-index: 1000"
+/>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import GoogleSSO from '$lib/components/GoogleSSO.svelte';
+</script>
+
+<div class="container h-screen mx-auto flex flex-col items-center justify-center">
+	<h1 class="text-3xl sm:text-4xl font-bold text-center mb-4">Welcome to Airel</h1>
+
+	<GoogleSSO />
+	<!-- {#each errors as error}
+		<pre>{error}</pre>
+	{/each} -->
+</div>
