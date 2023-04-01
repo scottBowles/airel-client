0bdaea912c92a64d46d7df9bc06a4f2e1a99a203
scottBowles <shbowles@gmail.com>
2023-04-01 02:54:46 -0400

    Add DateTime and GlobalID scalars to the houdini config

--- houdini.config.js
+++ houdini.config.js
@@ -2,12 +2,32 @@
 
 /** @type {import('houdini').ConfigFile} */
 const config = {
-    "watchSchema": {
-        "url": "http://127.0.0.1:8000/graphql/"
-    },
-    "plugins": {
-        "houdini-svelte": {}
-    }
-}
+	watchSchema: {
+		url: 'http://127.0.0.1:8000/graphql/'
+	},
+	plugins: {
+		'houdini-svelte': {}
+	},
+	scalars: {
+		DateTime: {
+			type: 'Date',
+			unmarshal(value) {
+				return new Date(value);
+			},
+			marshal(date) {
+				return date.getTime();
+			}
+		},
+		GlobalID: {
+			type: 'string',
+			unmarshal(value) {
+				return value;
+			},
+			marshal(value) {
+				return value;
+			}
+		}
+	}
+};
 
-export default config
+export default config;
