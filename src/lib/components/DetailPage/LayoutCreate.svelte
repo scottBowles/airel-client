7547f50df173e0fad15a10c49220ffb31215dd5b
scottBowles <shbowles@gmail.com>
2023-04-11 01:07:11 -0400

    Add functioning association create page

--- src/lib/components/DetailPage/LayoutCreate.svelte
+++ src/lib/components/DetailPage/LayoutCreate.svelte
@@ -4,59 +4,49 @@
 	import QuillEditor from '../QuillEditor.svelte';
 	import Spacer from '../Spacer.svelte';
 	import LayoutBase from './LayoutBase.svelte';
-
-	export let name: string | null = '';
-	export let imageIds: string[] = [];
-	export let onFormSubmit = () => {};
-	export let onImageUpload = () => {};
-	export let form: any = undefined;
 </script>
 
-<form on:submit|preventDefault={onFormSubmit}>
-	<LayoutBase clearfix>
-		<!-- NAME -->
-		<div slot="name" class="form-control">
-			<label for="name-input" class="label">
-				<span class="label-text">Name</span>
-			</label>
-			<input
-				type="text"
-				id="name-input"
-				name="name"
-				bind:value={$form.name}
-				class="input input-bordered w-full max-w-xs"
-				required
-			/>
-		</div>
+<LayoutBase clearfix>
+	<!-- NAME -->
+	<div slot="name" class="form-control">
+		<label for="name-input" class="label">
+			<span class="label-text">Name</span>
+		</label>
+		<input
+			type="text"
+			id="name-input"
+			name="name"
+			class="input input-bordered w-full max-w-xs"
+			required
+		/>
+	</div>
 
-		<!-- EDIT / SAVE + LOCKED BY {USER} -->
-		<button slot="lockedBy" class="ml-auto" type="submit">Save</button>
+	<!-- EDIT / SAVE + LOCKED BY {USER} -->
+	<button slot="lockedBy" class="ml-auto" type="submit">Save</button>
 
-		<!-- MAIN IMAGE -->
-		<div slot="mainImage" class="w-full max-w-xs mx-auto">
-			<CloudinaryUpload {onImageUpload}>
-				<ImageCarousel {imageIds} alt={name ?? ''} />
-			</CloudinaryUpload>
-			<Spacer />
-		</div>
+	<!-- MAIN IMAGE -->
+	<!-- <div slot="mainImage" class="w-full max-w-xs mx-auto">
+		<CloudinaryUpload {onImageUpload}>
+			<ImageCarousel {imageIds} alt={name ?? ''} />
+		</CloudinaryUpload>
+		<Spacer />
+	</div> -->
 
-		<!-- DESCRIPTION -->
-		<div slot="description" class="form-control">
-			<label for="description-input" class="label">
-				<span class="label-text">Description</span>
-			</label>
-			<textarea
-				name="description"
-				id="description-input"
-				bind:value={$form.description}
-				class="textarea textarea-bordered w-full max-w-xs"
-			/>
-		</div>
+	<!-- DESCRIPTION -->
+	<div slot="description" class="form-control">
+		<label for="description-input" class="label">
+			<span class="label-text">Description</span>
+		</label>
+		<textarea
+			name="description"
+			id="description-input"
+			class="textarea textarea-bordered w-full max-w-xs"
+		/>
+	</div>
 
-		<!-- PROPERTIES -->
-		<div slot="properties" />
+	<!-- PROPERTIES -->
+	<div slot="properties" />
 
-		<!-- MARKDOWN NOTES -->
-		<QuillEditor slot="markdownNotes" init="<p>Put initial content here</p>" />
-	</LayoutBase>
-</form>
+	<!-- MARKDOWN NOTES -->
+	<QuillEditor slot="markdownNotes" init="" />
+</LayoutBase>
--- src/routes/(protected)/associations/create/+page.svelte
+++ src/routes/(protected)/associations/create/+page.svelte
@@ -1,5 +1,42 @@
-<script>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { graphql } from '$houdini';
 	import { LayoutCreate } from '$lib/components/DetailPage';
+	import { error } from '@sveltejs/kit';
+	import { fromGlobalId } from 'graphql-relay';
+
+	const createMutation = graphql(`
+		mutation CreateAssociation($name: String!, $description: String, $markdownNotes: String) {
+			createAssociation(
+				input: { name: $name, description: $description, markdownNotes: $markdownNotes }
+			) {
+				... on Association {
+					id
+				}
+			}
+		}
+	`);
+
+	const handleSubmit = async (event: Event) => {
+		const data = new FormData(event.target as HTMLFormElement);
+		const name = data.get('name')?.toString();
+		const description = data.get('description')?.toString();
+		const markdownNotes = data.get('markdownNotes')?.toString();
+
+		if (!name) throw error(400, 'Name is required');
+
+		const res = await createMutation.mutate({ name, description, markdownNotes });
+
+		if (res.data) {
+			const { id: globalId } = res.data.createAssociation;
+			const { id } = fromGlobalId(globalId);
+			goto(`/associations/${id}`);
+		} else {
+			console.log('No data returned from createMutation.mutate! res.data: ', res.data);
+		}
+	};
 </script>
 
-<LayoutCreate>c</LayoutCreate>
+<form method="POST" on:submit|preventDefault={handleSubmit}>
+	<LayoutCreate />
+</form>
