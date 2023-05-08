a51a22e2d01ed76eeccb3fe532e4b0d00f51d10f
scottBowles <shbowles@gmail.com>
2023-05-07 21:34:13 -0400

    Console log to debug prod

--- src/lib/components/ItemListDisplay.svelte
+++ src/lib/components/ItemListDisplay.svelte
@@ -27,6 +27,8 @@
 	);
 
 	$: ({ id, name, weapon, armor, equipment } = $data);
+	console.log('first render fromGlobalId', fromGlobalId);
+	$: console.log({ fromGlobalId });
 	$: globalId = fromGlobalId(id).id;
 	$: href = `items/${globalId}`;
 </script>
