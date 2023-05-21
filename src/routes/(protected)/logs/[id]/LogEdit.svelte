3b49dc16542f8c597bc9b53418fc39424deb3729
scottBowles <shbowles@gmail.com>
2023-05-20 22:41:45 -0400

    Add found-in-this-log entities to log edit page

--- src/routes/(protected)/logs/[id]/LogEdit.svelte
+++ src/routes/(protected)/logs/[id]/LogEdit.svelte
@@ -9,6 +9,8 @@
 	import RelatedAssociationMultiSelect from '$lib/components/RelatedAssociationMultiSelect.svelte';
 	import RelatedCharacterMultiSelect from '$lib/components/RelatedCharacterMultiSelect.svelte';
 	import RelatedArtifactMultiSelect from '$lib/components/RelatedArtifactMultiSelect.svelte';
+	import { idFromEdge } from '../../places/utils';
+	import { somethingWentWrong } from '$lib/utils';
 
 	const unlockMutation = new UnlockStore();
 	const updateLog = new UpdateLogStore();
@@ -34,12 +36,61 @@
 						}
 					}
 				}
+				artifacts {
+					edges {
+						node {
+							id
+						}
+					}
+				}
+				associations {
+					edges {
+						node {
+							id
+						}
+					}
+				}
+				characters {
+					edges {
+						node {
+							id
+						}
+					}
+				}
+				items {
+					edges {
+						node {
+							id
+						}
+					}
+				}
+				places {
+					edges {
+						node {
+							id
+						}
+					}
+				}
+				races {
+					edges {
+						node {
+							id
+						}
+					}
+				}
 			}
 		`)
 	);
 
-	$: ({ id, title, gameDate, brief, synopsis, placesSetIn, lockUser } = $data);
-	$: initialPlaceIds = placesSetIn.edges.map((edge) => edge.node.id);
+	$: ({ id, title, gameDate, brief, synopsis, lockUser } = $data);
+
+	$: placesSetInIds = $data.placesSetIn.edges.map(idFromEdge);
+	$: initialArtifactIds = $data.artifacts.edges.map(idFromEdge);
+	$: initialAssociationIds = $data.associations.edges.map(idFromEdge);
+	$: initialCharacterIds = $data.characters.edges.map(idFromEdge);
+	$: initialItemIds = $data.items.edges.map(idFromEdge);
+	$: initialPlaceIds = $data.places.edges.map(idFromEdge);
+	$: initialRaceIds = $data.races.edges.map(idFromEdge);
 
 	const unlock = () => unlockMutation.mutate({ id });
 
@@ -47,127 +98,157 @@
 		const data = new FormData(event.target as HTMLFormElement);
 		const parsed = parseFormData(data);
 		const gameDate = parsed.gameDate ? new Date(parsed.gameDate as string) : null;
-
-		console.log({ parsed });
-		updateLog.mutate({ id, ...parsed, gameDate });
+		parsed.placesSetIn ??= { set: [] };
+		parsed.artifacts ??= { set: [] };
+		parsed.associations ??= { set: [] };
+		parsed.characters ??= { set: [] };
+		parsed.items ??= { set: [] };
+		parsed.places ??= { set: [] };
+		parsed.races ??= { set: [] };
+		const res = await updateLog.mutate({ id, ...parsed, gameDate });
+		if (res.errors) {
+			somethingWentWrong(res.errors[0].message);
+		}
 	};
 
 	const modalId = `discard-changes-modal-${id}`;
 </script>
 
-<div class="container mx-auto mt-8 mb-32 px-8">
-	<div class="flex justify-between items-center mb-8">
-		<h1 class="text-3xl font-bold">Edit Log</h1>
+<form method="POST" on:submit|preventDefault={handleSubmit}>
+	<div class="container mx-auto mt-8 mb-32 px-8">
+		<div class="flex justify-between items-center flex-wrap gap-4 mb-8">
+			<h1 class="text-3xl font-bold">Edit Log</h1>
 
-		<div class="flex items-center gap-8 min-w-fit">
-			<span>Locked by {lockUser?.username ?? 'Unknown'}</span>
+			<div class="flex items-center gap-8 min-w-fit">
+				<span>Locked by {lockUser?.username ?? 'Unknown'}</span>
 
-			<label for={modalId} class="btn btn-ghost btn-sm icon-btn modal-button"
-				><div class="tooltip" data-tip="Discard changes">
-					<span class="icon"><FaUndoAlt /></span>
-				</div></label
-			>
-			<input type="checkbox" id={modalId} class="modal-toggle" />
-			<label for={modalId} class="modal modal-bottom sm:modal-middle cursor-pointer">
-				<label class="modal-box relative" for="">
-					<h3 class="font-bold text-lg">Discard changes</h3>
-					<p class="py-4">Are you sure you want to discard any unsaved changes?</p>
-					<div class="modal-action">
-						<label for={modalId} class="btn" on:click={unlock} on:keypress={unlock}>Yes</label>
-						<label for={modalId} class="btn">No</label>
-					</div>
+				<label for={modalId} class="btn btn-ghost btn-sm icon-btn modal-button"
+					><div class="tooltip" data-tip="Discard changes">
+						<span class="icon"><FaUndoAlt /></span>
+					</div></label
+				>
+				<input type="checkbox" id={modalId} class="modal-toggle" />
+				<label for={modalId} class="modal modal-bottom sm:modal-middle cursor-pointer">
+					<label class="modal-box relative" for="">
+						<h3 class="font-bold text-lg">Discard changes</h3>
+						<p class="py-4">Are you sure you want to discard any unsaved changes?</p>
+						<div class="modal-action">
+							<label for={modalId} class="btn" on:click={unlock} on:keypress={unlock}>Yes</label>
+							<label for={modalId} class="btn">No</label>
+						</div>
+					</label>
 				</label>
-			</label>
 
-			<button type="submit" class="btn btn-ghost btn-sm icon-btn"
-				><div class="tooltip" data-tip="Save"><span class="icon"><FaSave /></span></div></button
-			>
+				<button type="submit" class="btn btn-ghost btn-sm icon-btn"
+					><div class="tooltip" data-tip="Save"><span class="icon"><FaSave /></span></div></button
+				>
+			</div>
+		</div>
+		<hr class="mb-8" />
+
+		<div class="flex flex-col sm:flex-row gap-16">
+			<div class="flex flex-col gap-2 flex-[2]">
+				<h2 class="text-xl font-bold my-4">Details</h2>
+
+				<div class="form-control">
+					<label for="title-input" class="label">
+						<span class="label-text">Title</span>
+					</label>
+					<input
+						type="text"
+						id="title-input"
+						name="title"
+						value={title}
+						class="input input-bordered w-full max-w-sm"
+						required
+					/>
+				</div>
+				<div class="form-control">
+					<label for="game-date" class="label">
+						<span class="label-text">Game Date</span>
+					</label>
+					<input
+						type="date"
+						id="game-date"
+						name="gameDate"
+						value={gameDate?.toISOString().substring(0, 10) ?? ''}
+						class="input input-bordered w-full max-w-sm"
+						required
+					/>
+				</div>
+				<div class="form-control">
+					<label for="brief" class="label">
+						<span class="label-text">Brief</span>
+					</label>
+					<textarea
+						id="brief"
+						name="brief"
+						class="textarea textarea-bordered w-full"
+						value={brief}
+						required
+					/>
+				</div>
+				<div class="form-control">
+					<label for="synopsis" class="label">
+						<span class="label-text">Synopsis</span>
+					</label>
+					<textarea
+						id="synopsis"
+						name="synopsis"
+						class="textarea textarea-bordered w-full"
+						value={synopsis}
+						required
+					/>
+				</div>
+				<div class="max-w-xs">
+					<RelatedPlaceMultiSelect
+						id={`log-${id}-place-select`}
+						inputGroupName="placesSetIn"
+						displayName="Places Set In"
+						initialPlaceIds={placesSetInIds}
+					/>
+				</div>
+			</div>
+
+			<div class="flex flex-col items-center flex-1">
+				<h2 class="text-xl font-bold my-4">Found in this Log</h2>
+
+				<div class="flex flex-col gap-2 w-full">
+					<RelatedArtifactMultiSelect
+						id="related-artifact-select"
+						inputGroupName="artifacts"
+						{initialArtifactIds}
+					/>
+					<RelatedAssociationMultiSelect
+						id="related-association-select"
+						inputGroupName="associations"
+						{initialAssociationIds}
+					/>
+					<RelatedCharacterMultiSelect
+						id="related-character-select"
+						inputGroupName="characters"
+						{initialCharacterIds}
+					/>
+					<RelatedItemMultiSelect
+						id="related-item-select"
+						inputGroupName="items"
+						{initialItemIds}
+					/>
+					<RelatedPlaceMultiSelect
+						id="related-place-select"
+						inputGroupName="places"
+						{initialPlaceIds}
+					/>
+					<RelatedRaceMultiSelect
+						id="related-race-select"
+						inputGroupName="races"
+						{initialRaceIds}
+					/>
+				</div>
+			</div>
 		</div>
 	</div>
-	<hr class="mb-8" />
-
-	<form
-		method="POST"
-		on:submit|preventDefault={handleSubmit}
-		class="flex flex-col sm:flex-row gap-16"
-	>
-		<div class="flex flex-col gap-2">
-			<h2 class="text-xl font-bold my-4">Details</h2>
-
-			<div class="form-control">
-				<label for="title-input" class="label">
-					<span class="label-text">Title</span>
-				</label>
-				<input
-					type="text"
-					id="title-input"
-					name="title"
-					value={title}
-					class="input input-bordered w-full max-w-sm"
-					required
-				/>
-			</div>
-			<div class="form-control">
-				<label for="game-date" class="label">
-					<span class="label-text">Game Date</span>
-				</label>
-				<input
-					type="date"
-					id="game-date"
-					name="gameDate"
-					value={gameDate?.toISOString().substring(0, 10) ?? ''}
-					class="input input-bordered w-full max-w-sm"
-					required
-				/>
-			</div>
-			<div class="form-control">
-				<label for="brief" class="label">
-					<span class="label-text">Brief</span>
-				</label>
-				<textarea
-					id="brief"
-					name="brief"
-					class="textarea textarea-bordered w-full"
-					value={brief}
-					required
-				/>
-			</div>
-			<div class="form-control">
-				<label for="synopsis" class="label">
-					<span class="label-text">Synopsis</span>
-				</label>
-				<textarea
-					id="synopsis"
-					name="synopsis"
-					class="textarea textarea-bordered w-full"
-					value={synopsis}
-					required
-				/>
-			</div>
-			<div class="max-w-xs">
-				<RelatedPlaceMultiSelect
-					id={`log-${id}-place-select`}
-					name="placesSetIn"
-					displayName="Places Set In"
-					{initialPlaceIds}
-				/>
-			</div>
-		</div>
-
-		<div class="flex flex-col items-center">
-			<h2 class="text-xl font-bold my-4">Found in this Log</h2>
-
-			<div class="flex flex-col gap-2 w-full">
-				<RelatedCharacterMultiSelect />
-				<RelatedItemMultiSelect />
-				<RelatedArtifactMultiSelect />
-				<RelatedPlaceMultiSelect />
-				<RelatedAssociationMultiSelect />
-				<RelatedRaceMultiSelect />
-			</div>
-		</div>
-	</form>
-</div>
+</form>
 
 <style>
 	.icon {
