0bb2aeaf7db5dadfe17aa324e801d2f31cc3c6c8
scottBowles <shbowles@gmail.com>
2023-06-06 02:04:17 -0400

    Move token refresh to plugin so it refreshes immediately

--- src/client.ts
+++ src/client.ts
@@ -1,41 +1,49 @@
-import jwt_decode from 'jwt-decode';
-import { HoudiniClient } from '$houdini';
-import refreshAuthToken from '$lib/customApiCalls/refreshAuthToken';
+import { HoudiniClient, type ClientPlugin } from '$houdini';
 import { PUBLIC_GRAPHQL_URL } from '$env/static/public';
+import jwt_decode from 'jwt-decode';
+import refreshAuthToken from '$lib/customApiCalls/refreshAuthToken';
+
+export const authRefreshPlugin: ClientPlugin = () => {
+	return {
+		async network(ctx, { next }) {
+			const { token, refresh_token } = ctx.session ?? {};
+			if (ctx.session && token && refresh_token) {
+				const decodedJon = jwt_decode(token) as { payload: string };
+				const decoded = JSON.parse(decodedJon.payload) as {
+					exp: string;
+					origIat: string;
+					username: string;
+				};
+				const timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
+				const buffer = 15 * 1000;
+				const now = new Date().valueOf();
+				const exp = new Date(decoded.exp).valueOf() - timezoneOffset - buffer;
+				const shouldRefresh = exp < now;
+
+				if (shouldRefresh) {
+					const res = await refreshAuthToken(refresh_token);
+					if (res.success) {
+						ctx.session.token = res.token.token;
+						ctx.session.refresh_token = res.refreshToken.token;
+						ctx.session.user = res.token.payload.username;
+						if (!ctx.fetchParams) ctx.fetchParams = {};
+						ctx.fetchParams.headers = {
+							Authorization: `JWT ${res.token.token}`
+						};
+					} else {
+						ctx.session.user = undefined;
+					}
+				}
+			}
+			next(ctx);
+		}
+	};
+};
 
 export default new HoudiniClient({
 	url: PUBLIC_GRAPHQL_URL,
-
-	// uncomment this to configure the network call (for things like authentication)
-	// for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
+	plugins: [authRefreshPlugin],
 	fetchParams({ session }) {
-		// if we have a token, check if it's expired and refresh it if it is
-		if (session?.token && session?.refresh_token) {
-			const decodedJson = jwt_decode(session.token) as { payload: string };
-			const decoded = JSON.parse(decodedJson.payload) as {
-				exp: string;
-				origIat: string;
-				username: string;
-			};
-			const timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
-			const buffer = 15 * 1000;
-			const now = new Date().valueOf();
-			const exp = new Date(decoded.exp).valueOf() - timezoneOffset - buffer;
-			const shouldRefresh = exp < now;
-
-			if (shouldRefresh) {
-				refreshAuthToken(session.refresh_token).then((res) => {
-					if (res.success) {
-						session.token = res.token.token;
-						session.refresh_token = res.refreshToken.token;
-						session.user = res.token.payload.username;
-					} else {
-						session.user = undefined;
-					}
-				});
-			}
-		}
-
 		return {
 			headers: {
 				Authorization: `JWT ${session?.token}`
