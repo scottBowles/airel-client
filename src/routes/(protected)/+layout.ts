5d28ba36bdb138d9c6cc36b0a739438e2020bc11
scottBowles <shbowles@gmail.com>
2023-06-06 02:01:01 -0400

    Remove unused

--- src/routes/(protected)/+layout.ts
+++ src/routes/(protected)/+layout.ts
@@ -1,14 +1,7 @@
 import { MeStore } from '$houdini';
-import { redirect } from '@sveltejs/kit';
 
 export const load = async (event) => {
 	const MeQuery = new MeStore();
-
 	const result = await MeQuery.fetch({ event });
-
-	// if (result.errors) {
-	// 	throw redirect(301, `/login?redirect=${event.url.pathname}`);
-	// }
-
 	return { me: result.data?.me };
 };
