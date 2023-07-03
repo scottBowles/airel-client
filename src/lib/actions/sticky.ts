dc6036d37f7a61be416d2726326dc02ce3688c32
scottBowles <shbowles@gmail.com>
2023-07-03 00:31:22 -0400

    Add sticky planet display on logs page

---
+++ src/lib/actions/sticky.ts
@@ -0,0 +1,33 @@
+// https://css-tricks.com/how-to-detect-when-a-sticky-element-gets-pinned/
+
+import type { Action } from 'svelte/action';
+
+type Sticky = Action<
+	HTMLDivElement,
+	never,
+	{ 'on:stuck': (e: CustomEvent<{ isStuck: boolean }>) => void }
+>;
+
+const sticky: Sticky = (node: HTMLElement) => {
+	const intersectionCallback = function (entries: IntersectionObserverEntry[]) {
+		const isStuck = entries[0].intersectionRatio < 1;
+
+		node.dispatchEvent(
+			new CustomEvent('stuck', {
+				detail: { isStuck }
+			})
+		);
+	};
+
+	const observer = new IntersectionObserver(intersectionCallback, { threshold: [1] });
+
+	observer.observe(node);
+
+	return {
+		destroy() {
+			observer.disconnect();
+		}
+	};
+};
+
+export default sticky;
