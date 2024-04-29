62f192dbc599b4e139ac8dffe68898167259fbf3
scottBowles <shbowles@gmail.com>
2024-04-28 23:28:49 -0400

    Update @typescript-eslint/no-unused-vars rule to allow underscore-prefixed unused vars

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,6 +32,18 @@ module.exports = {
 		node: true
 	},
 	rules: {
-		'no-inner-declarations': 'off'
+		'no-inner-declarations': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				args: 'all',
+				argsIgnorePattern: '^_',
+				caughtErrors: 'all',
+				caughtErrorsIgnorePattern: '^_',
+				destructuredArrayIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				ignoreRestSiblings: true
+			}
+		]
 	}
 };
