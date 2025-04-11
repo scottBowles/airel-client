cb1807a73b69cb251ff5c3d661032f04bec2e43e
scottBowles <shbowles@gmail.com>
2025-04-10 20:25:54 -0400

    Add some logs

--- src/routes/(protected)/+layout.ts
+++ src/routes/(protected)/+layout.ts
@@ -1,7 +1,10 @@
 import { MeStore } from '$houdini';
 
 export const load = async (event) => {
+	console.log('routes/(protected)/+layout.ts: load function 1');
 	const MeQuery = new MeStore();
+	console.log('routes/(protected)/+layout.ts: load function 2');
 	const result = await MeQuery.fetch({ event });
+	console.log('routes/(protected)/+layout.ts: load function 3');
 	return { me: result.data?.me };
 };
